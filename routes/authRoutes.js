6eab40d57125551fdc9f9739eabc8354b83b0efa
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-08-10 19:02:19 +0100

    refactor auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,27 +1,26 @@
-const passport = require('passport');
+const passport = require("passport")
+const express = require("express")
 
-module.exports = app => {
-  app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
-    })
-  );
+const router = express.Router()
 
-  app.get(
-    '/auth/google/callback',
-    passport.authenticate('google'),
-    (req, res) => {
-      res.redirect('/blogs');
-    }
-  );
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+)
 
-  app.get('/auth/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
-  });
+router.get("/google/callback", passport.authenticate("google"), (req, res) => {
+  res.redirect("/blogs")
+})
 
-  app.get('/api/current_user', (req, res) => {
-    res.send(req.user);
-  });
-};
+router.get("/logout", (req, res) => {
+  req.logout()
+  res.redirect("/")
+})
+
+router.get("/current_user", (req, res) => {
+  res.send(req.user)
+})
+
+module.exports = router
