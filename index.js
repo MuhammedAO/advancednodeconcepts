986c7d4a181a5b19f1933e8e99d4565ba6d61bdf
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-08-10 18:29:29 +0100

    server

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cookieSession = require("cookie-session")
+const passport = require("passport")
+const keys = require("./config/keys")
+
+require("./models/User")
+require("./models/Blog")
+require("./services/passport")
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+  .then(() => console.log('Db Connected'))
+  .catch((err) => console.log(err))
+
+const app = express()
+
+app.use(express.json())
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require("./routes/authRoutes")(app)
+require("./routes/blogRoutes")(app)
+
+if (process.env.NODE_ENV === "production") {
+
+  // Set static folder   
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static("client/build"))
+
+  // index.html for all page routes,  html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+  });
+}
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT)
+})
