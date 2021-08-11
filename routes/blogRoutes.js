1ac88f027dde86840359f93abe9b498e357cc6fb
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-08-11 23:30:32 +0100

    revert changes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -15,28 +15,10 @@ router.get("/:id", requireLogin, async (req, res) => {
 })
 
 router.get("/", requireLogin, async (req, res) => {
-  const redis = require("redis")
-  const redisUrl = "redis://127.0.0.1:16379"
-  const client = redis.createClient(redisUrl)
-  const util = require("util")
 
-  client.get = util.promisify(client.get)
-
-  //check for cached data in redis related to this query.
-  const cachedBlog = await client.get(req.user.id)
-
-  if (cachedBlog) {
-    console.log('Serving from cache')
-
-    return res.send(JSON.parse(cachedBlog))
-  }
-
-  //if no, respond to the request normally and update the cache by storing the data
   const blogs = await Blog.find({ _user: req.user.id })
-  console.log('Serving from mongoDB')
+  
   res.send(blogs)
-
-  client.set(req.user.id, JSON.stringify(blogs))
 })
 
 router.post("/", requireLogin, async (req, res) => {
