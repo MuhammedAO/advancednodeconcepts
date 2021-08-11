75283126abbbda1b1d4dedb0a635d2ed68321b23
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-08-12 00:00:10 +0100

    hydrate models

--- services/cache.js
+++ services/cache.js
@@ -1,32 +1,38 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const redis = require("redis")
-const util = require('util')
+const util = require("util")
 
 const redisUrl = "redis://127.0.0.1:16379"
 const client = redis.createClient(redisUrl)
 
 client.get = util.promisify(client.get)
 
-
 const exec = mongoose.Query.prototype.exec
 
-mongoose.Query.prototype.exec = async function(){
-  console.log('about to run a query')
+//runs before any mongoose query get executed.
+mongoose.Query.prototype.exec = async function () {
+  console.log("about to run a query")
 
- const key = JSON.stringify(Object.assign({}, this.getQuery(), {
-   collection: this.mongooseCollection.name
- }))
- 
- //check for a value of 'key' in redis
- const cachedValue = await client.get(key)
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  )
 
- //if found, return
- if(cachedValue){
-   console.log(cachedValue)
+  //check for a value of 'key' in redis
+  const cachedValue = await client.get(key)
 
-   return JSON.parse(cachedValue)
- }
- //if !found, issue the query and store the result in redis
+  //if found, return
+  if (cachedValue) {
+    console.log(cachedValue)
+
+    const doc = JSON.parse(cachedValue)
+
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc)
+  }
+  //if !found, issue the query and store the result in redis
 
   const result = await exec.apply(this, arguments)
   client.set(key, JSON.stringify(result))
